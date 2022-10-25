c12c174c03565b4195848b1f9f82d706c1b32b06
Everton <everton.h.g.fonseca@gmail.com>
2022-10-25 12:18:50 -0300

    migrations e dependencias do front

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Thread extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }

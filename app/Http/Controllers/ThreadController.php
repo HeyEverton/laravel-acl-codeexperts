43130c70024e6fd286e9288bc676741a7e1f2a0d
Everton <everton.h.g.fonseca@gmail.com>
2022-10-25 12:24:41 -0300

    threadcontroller com resource (sem validacao)

---
+++ app/Http/Controllers/ThreadController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Thread;
+use Illuminate\Http\Request;
+
+class ThreadController extends Controller
+{
+
+    public function __construct(private Thread $thread)
+    {
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $threads = $this->thread->paginate(15);
+
+        return view('thread.index', compact('threads'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('thread.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $this->thread->create($request->all());
+            dd('topico criado');
+        } catch (\Throwable $th) {
+            dd('error');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $thread = $this->thread->find($id);
+
+        return $thread;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $thread = $this->thread->find($id);
+        return view('thread.edit', compact('thread'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        try {
+            $thread = $this->thread->find($id);
+            $thread->update($request->all());
+
+            dd('topico atualizado');
+        } catch (\Throwable $th) {
+            dd('error');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            $thread = $this->thread->find($id);
+            $thread->delete();
+
+            dd('topico excluido');
+        } catch (\Throwable $th) {
+            dd('error');
+        }
+    }
+}

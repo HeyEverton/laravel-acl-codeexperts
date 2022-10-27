0067c93e23a2d96299a4ac65358740b5d1f343de
Everton <everton.h.g.fonseca@gmail.com>
2022-10-27 14:24:48 -0300

    migrations de roles, resources e relacoes estabelecidas

---
+++ app/Models/Resource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Resource extends Model
+{
+    use HasFactory;
+    // protected $table = 'resources';
+
+    protected $fillable = [
+        'name',
+        'resource',
+        'is_menu',
+    ];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+}
